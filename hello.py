dc36ec4e9466e3199e169019ca06e590846d1b04
Edwin Wang <edwinwang62@gmail.com>
2025-11-08 22:01:38 +0300

    Update

---
+++ hello.py
@@ -0,0 +1,25 @@
+# #name=input("Enter Your name: ")
+# #hobby=input("Your hobby: ")
+# greetings="Yamune"
+# illegal ="We are the \"nerds\" of East Africa"
+
+# #print(f'Your name is {name} and you love {hobby}')
+# print(greetings[2:])
+
+# print(illegal)
+
+# print(greetings[-5:-2])
+
+# DataStructures
+# Lists
+names =["Priscilla","Jane","Nick", "Debra","Jane"]
+
+# Acess items using index
+
+print(names[0])
+print(names[-1])
+print(len(names))
+
+
+
+
